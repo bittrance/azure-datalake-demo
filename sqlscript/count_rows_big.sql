4498093f44af65f858946630b79eb16437768b4f
Anders Qvist <anders.qvist@xenit.se>
2021-12-02 10:15:41 +0100

    More Synapse conventions.
    Signed-off-by: Anders Qvist <anders.qvist@xenit.se>

--- sqlscript/count_rows_big.sql
+++
@@ -1,9 +0,0 @@
-select count(*)
-from openrowset(
-   bulk 'https://bittrancedatalakedemo.dfs.core.windows.net/big-data/big-set/',
-   format = 'csv',
-   fieldterminator ='0x0b',
-   fieldquote = '0x0b',
-   rowterminator = '0x0a'
-    ) with (doc nvarchar(max)) as rows
-    cross apply openjson(doc, '$') with ("id" varchar(20))
