a8ff459e19059e88532ca3b9ad06af9af634d48e
Anders Qvist <anders.qvist@xenit.se>
2022-03-04 13:48:37 +0100

    Skip partition initialization in client and switch to persisting offsets in storage account.

--- eventhub-client/client.py
+++ eventhub-client/client.py
@@ -1,56 +1,34 @@
 #!/usr/bin/env python
 
 import os
-from azure.eventhub import EventHubConsumerClient
 
 CONNECTION_STR = os.environ["EVENT_HUB_CONNECTION_STRING"]
 EVENTHUB_NAME = os.environ['EVENT_HUB_NAME']
+BLOB_CONNECTION_STR = os.environ["BLOB_CONNECTION_STRING"]
+CONTAINER_NAME = os.environ["CONTAINER_NAME"]
 
-def on_event(partition_context, event):
-    # Put your code here.
-    # If the operation is i/o intensive, multi-thread will have better performance.
-    print("Received event from partition: {}: {}".format(partition_context.partition_id, event))
-    partition_context.update_checkpoint(event)
-
-def on_partition_initialize(partition_context):
-    # Put your code here.
-    print("Partition: {} has been initialized.".format(partition_context.partition_id))
+import asyncio
+from azure.eventhub.aio import EventHubConsumerClient
+from azure.eventhub.extensions.checkpointstoreblobaio import BlobCheckpointStore
 
 
-def on_partition_close(partition_context, reason):
-    # Put your code here.
-    print("Partition: {} has been closed, reason for closing: {}.".format(
-        partition_context.partition_id,
-        reason
-    ))
+async def on_event(partition_context, event):
+    # Print the event data.
+    print("Received the event: \"{}\" from the partition with ID: \"{}\"".format(event.body_as_str(encoding='UTF-8'), partition_context.partition_id))
 
+    # Update the checkpoint so that the program doesn't read the events
+    # that it has already read when you run it next time.
+    await partition_context.update_checkpoint(event)
 
-def on_error(partition_context, error):
-    # Put your code here. partition_context can be None in the on_error callback.
-    if partition_context:
-        print("An exception: {} occurred during receiving from Partition: {}.".format(
-            partition_context.partition_id,
-            error
-        ))
-    else:
-        print("An exception: {} occurred during the load balance process.".format(error))
+async def main():
+    # Create an Azure blob checkpoint store to store the checkpoints.
+    checkpoint_store = BlobCheckpointStore.from_connection_string(BLOB_CONNECTION_STR, CONTAINER_NAME)
 
+    # Create a consumer client for the event hub.
+    client = EventHubConsumerClient.from_connection_string(CONNECTION_STR, consumer_group="$Default", eventhub_name=EVENTHUB_NAME, checkpoint_store=checkpoint_store)
+    async with client:
+        # Call the receive method. Read from the beginning of the partition (starting_position: "-1")
+        await client.receive(on_event=on_event,  starting_position="-1")
 
 if __name__ == '__main__':
-    consumer_client = EventHubConsumerClient.from_connection_string(
-        conn_str=CONNECTION_STR,
-        consumer_group='$Default',
-        eventhub_name=EVENTHUB_NAME,
-    )
-
-    try:
-        with consumer_client:
-            consumer_client.receive(
-                on_event=on_event,
-                # on_partition_initialize=on_partition_initialize,
-                # on_partition_close=on_partition_close,
-                on_error=on_error,
-                starting_position="-1",  # "-1" is from the beginning of the partition.
-            )
-    except KeyboardInterrupt:
-        print('Stopped receiving.')
+    asyncio.run(main())
--- iot-storage-eventhub-demo/README.md
+++ iot-storage-eventhub-demo/README.md
@@ -2,9 +2,17 @@
 
 az extension add --name azure-iot
 
+az iot hub device-identity create --hub-name bittrance-iot-demo-hub --device-id iot-demo-test-1
+
 az iot hub generate-sas-token --resource-group iot-demo --hub-name bittrance-iot-demo-hub --device-id iot-demo-test-1
 
 mosquitto_pub -d -h bittrance-iot-demo-hub.azure-devices.net -p 8883 -i iot-demo-test-1 -u "bittrance-iot-demo-hub.azure-devices.net/iot-demo-test-1/?api-version=2018-06-30" -P "<sas-token>" -t "devices/iot-demo-test-1/messages/events/" -m "hello world" -V mqttv311  -q 1
 
 az storage blob list --account-name bittranceiotdemo --container-name iot-demo-container --output table
 
+
+az eventhubs eventhub authorization-rule keys list --namespace-name iot-demo-namespace --name listen-rule --resource-group iot-demo --eventhub-name iot-demo-eventhub
+
+az storage account show-connection-string --resource-group iot-demo --name bittranceiotdemo
+
+env EVENT_HUB_NAME=iot-demo-eventhub EVENT_HUB_CONNECTION_STRING='...' BLOB_CONNECTION_STRING='...' CONTAINER_NAME='iot-demo-container' ./eventhub-client/client.py
