52b1ff84e3d879a83e1f98cd402dc307cfda325e
Bittrance <bittrance@gmail.com>
2024-04-12 21:07:02 +0200

    Example App has OAuth2 authn, but no authz (allowing any valid user/sp to connect).

--- api-management-auth-proxy/function.tf
+++ api-management-auth-proxy/function.tf
@@ -34,9 +34,90 @@ resource "azurerm_linux_function_app" "app" {
     application_insights_key = azurerm_application_insights.insights.instrumentation_key
   }
 
+  identity {
+    type = "SystemAssigned"
+  }
+
+  auth_settings_v2 {
+    auth_enabled           = true
+    require_authentication = true
+    unauthenticated_action = "Return401"
+
+    login {}
+
+    active_directory_v2 {
+      client_id                  = azuread_application.app.application_id
+      client_secret_setting_name = "APPCLIENTSECRET"
+      tenant_auth_endpoint       = "https://login.microsoftonline.com/${data.azurerm_client_config.current.tenant_id}/v2.0"
+    }
+  }
+
   lifecycle {
     ignore_changes = [
       tags,
     ]
   }
+
+  depends_on = [azurerm_key_vault_secret.app_secret]
+}
+
+resource "azurerm_key_vault" "kv" {
+  name                      = "bittrancehelloapp"
+  location                  = azurerm_resource_group.rg.location
+  resource_group_name       = azurerm_resource_group.rg.name
+  sku_name                  = "standard"
+  tenant_id                 = data.azurerm_client_config.current.tenant_id
+  enable_rbac_authorization = true
+}
+
+resource "azurerm_role_assignment" "self_write" {
+  scope                = azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Administrator"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
+
+resource "azurerm_role_assignment" "app_kv_read" {
+  scope                = azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = azurerm_linux_function_app.app.identity[0].principal_id
+}
+
+resource "random_string" "app_secret" {
+  length  = 40
+  special = true
+}
+
+resource "azurerm_key_vault_secret" "app_secret" {
+  name         = "APPCLIENTSECRET"
+  value        = random_string.app_secret.result
+  key_vault_id = azurerm_key_vault.kv.id
+  depends_on   = [azurerm_role_assignment.self_write]
+}
+
+resource "random_uuid" "permission_id" {}
+
+resource "azuread_application" "app" {
+  display_name = "bittrance-test-hello"
+
+  api {
+    requested_access_token_version = 2
+
+    oauth2_permission_scope {
+      admin_consent_description  = "."
+      admin_consent_display_name = "Hello Function App"
+      enabled                    = true
+      id                         = random_uuid.permission_id.result
+      type                       = "Admin"
+      user_consent_description   = "."
+      user_consent_display_name  = "Hello Function App"
+      value                      = "use"
+    }
+  }
+}
+
+# Convenient to get test creds with `az account get-access-token --resource XXX`
+resource "azuread_application_pre_authorized" "azcli" {
+  application_id       = azuread_application.app.id
+  authorized_client_id = "04b07795-8ddb-461a-bbee-02f9e1bf7b46"
+  permission_ids       = [random_uuid.permission_id.result]
 }
